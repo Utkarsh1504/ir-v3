352a86d80be228eb9c84cb354c9bcb1e1da68f70
Utkarsh1504 <utkarshmishra73444@gmail.com>
2021-08-08 10:36:21 +0530

    fixed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
+import State from "./State";
+import Effect from "./Effect";
+import Context from "./Context";
+import Ref from "./Ref";
+import Reducer from "./Reducer";
+import Memo from "./Memo";
+import Callback from "./Callback";
+import LayoutEffect from "./LayoutEffect";
+import ImperativeHandle from "./ImperativeHandle";
+import DebugValue from "./DebugValue";
+
+import './index.css';
 
 function App() {
   return (
-    <div>
-      <h1>Hi this is Intermediate react v3</h1>
+    <div className="App">
+      <State />
+      <hr />
+      <Effect />
+      <hr />
+      <Context />
+      <hr />
+      <Ref />
+      <hr />
+      <Reducer />
+      <hr />
+      <Memo />
+      <hr />
+      <Callback />
+      <hr />
+      <LayoutEffect />
+      <hr />
+      <ImperativeHandle />
+      <hr />
+      <DebugValue />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,42 +1,11 @@
-import ReactDOM from "react-dom";
-import State from "./State";
-import Effect from "./Effect";
-import Context from "./Context";
-import Ref from "./Ref";
-import Reducer from "./Reducer";
-import Memo from "./Memo";
-import Callback from "./Callback";
-import LayoutEffect from "./LayoutEffect";
-import ImperativeHandle from "./ImperativeHandle";
-import DebugValue from "./DebugValue";
+import {render} from "react-dom";
+import App from "./App";
 
-import './index.css';
-
-function App() {
+function Home() {
   return (
-    <div className="App">
-      <State />
-      <hr />
-      <Effect />
-      <hr />
-      <Context />
-      <hr />
-      <Ref />
-      <hr />
-      <Reducer />
-      <hr />
-      <Memo />
-      <hr />
-      <Callback />
-      <hr />
-      <LayoutEffect />
-      <hr />
-      <ImperativeHandle />
-      <hr />
-      <DebugValue />
-    </div>
+    <App />
   );
 }
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+render(<Home />, rootElement);
